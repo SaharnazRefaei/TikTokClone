5b704a875448df3041366f9888cb5d47c9763c7f
Saharnaz <saharnaz.refaei@gmail.com>
2022-09-03 13:16:43 +0430

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: [
+			'i.picsum.photos',
+			'www.scmp.com',
+			'lh3.googleusercontent.com',
+		],
+	},
+};
+
+module.exports = nextConfig;
